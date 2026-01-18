e99de9c4b5af179a110d657e35febc2e1b1e419e
Marcus <marcus.drummer@live.com>
2026-01-18 14:29:40 -0300

    fix js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,5 +1,7 @@
 const API_BASE_URL =
-  window.location.hostname === "localhost" ? "http://localhost:8000" : "/api";
+  window.location.hostname === "localhost"
+    ? "http://localhost:8000"
+    : "https://autou-desafio-production.up.railway.app/";
 
 const analyzeBtn = document.getElementById("analyzeBtn");
 const fileInput = document.getElementById("fileInput");
